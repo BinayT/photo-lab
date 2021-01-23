9b46a283c3e650eb10b7801acf86148f8a8e3a5c
Binay <binayprogramming@gmail.com>
2021-01-24 00:13:25 +0100

    Server now listening to either enviormental port or local port

--- server/index.js
+++ server/index.js
@@ -14,3 +14,6 @@ const app = express();
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('Server running on port ' + PORT));
