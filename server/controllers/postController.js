c12aae0e1422d526b5e15839f2eb6f7873cbaf00
Binay <binayprogramming@gmail.com>
2021-01-25 01:25:15 +0100

    Controller to create a post made as well

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,16 +1,24 @@
-import postModel from '../models/postModel.js';
+import PostModel from '../models/postModel.js';
 
 const getPosts = async (req, res) => {
   try {
-    const posts = await postModel.find({});
+    const posts = await PostModel.find({});
     res.status(200).json(posts);
   } catch (error) {
     res.status(500).json({ msg: error.message });
   }
 };
 
-const createPost = (req, res) => {
-  res.json({ msg: 'Post Creation' });
+const createPost = async (req, res) => {
+  const postBody = req.body;
+
+  const newPost = new PostModel(postBody);
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ msg: error.message });
+  }
 };
 
 export { getPosts, createPost };
