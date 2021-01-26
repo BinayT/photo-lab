65ee933dc1189cb39dbf7311c76a49f0adba65cf
Binay <binayprogramming@gmail.com>
2021-01-26 23:26:01 +0100

    Added the form and post components in the home screen, with responsiveness with the help of material-ui

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,6 +1,8 @@
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
 import photolab from '../images/photolab.png';
+import Posts from '../components/Posts/Posts';
+import Form from '../components/Form/Form';
 
 const Home = () => {
   return (
@@ -11,6 +13,23 @@ const Home = () => {
         </Typography>
         <img src={photolab} alt='photolab logo' height='60' />
       </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify='space-between'
+            alignItems='stretch'
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
     </Container>
   );
 };
