64074a34ad7ea926a7b1acc46a1f82380decfe8c
Binay <binayprogramming@gmail.com>
2021-01-26 23:38:58 +0100

    Changed the logo and made the navbar look way prettier

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,17 +1,19 @@
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
-import photolab from '../images/photolab.png';
+import camera from '../images/camera.jpg';
 import Posts from '../components/Posts/Posts';
 import Form from '../components/Form/Form';
+import useStyles from './styles';
 
 const Home = () => {
+  const classes = useStyles();
   return (
     <Container maxwidth='lg'>
-      <AppBar position='static' color='inherit'>
+      <AppBar className={classes.appBar} position='static' color='inherit'>
         <Typography variant='h2' align='center'>
           Photo Lab
         </Typography>
-        <img src={photolab} alt='photolab logo' height='60' />
+        <img src={camera} alt='camera logo' height='60' />
       </AppBar>
       <Grow in>
         <Container>
