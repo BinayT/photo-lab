2585fa0c810de2e727b8c64c1348a6f37c47027b
Binay <binayprogramming@gmail.com>
2021-01-26 23:57:41 +0100

    Store imported in index.js file and implemented global state

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'react-thunk';
+import thunk from 'redux-thunk';
 
 import postReducer from './reducers/postReducer';
 
