6452c5a87af460fc63d24f29cee77003cfb38820
Binay <binayprogramming@gmail.com>
2021-01-26 23:22:37 +0100

    Made the app's logo and imported it and it's rendering correctly

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,11 +1,13 @@
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
+import photolab from '../images/photolab.png';
+
 const Home = () => {
   return (
     <Container maxwidth='lg'>
       <AppBar position='static' color='inherit'>
         <Typography variant='h2' align='center'>
-          Photo-Lab
+          Photo Lab
         </Typography>
         <img src={photolab} alt='photolab logo' height='60' />
       </AppBar>
