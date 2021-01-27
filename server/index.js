76b350c0909ccc728d3b448783f9a19ccb450e11
Binay <binayprogramming@gmail.com>
2021-01-27 23:43:39 +0100

    Had used cors after actually sending the routes, fixed the cors issue

--- server/index.js
+++ server/index.js
@@ -10,11 +10,11 @@ dotenv.config();
 connectDB();
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('Server running on port ' + PORT));
