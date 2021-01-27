68b6ba9c46cdb17398c0f05630d9913dd55307f4
Binay <binayprogramming@gmail.com>
2021-01-27 23:22:28 +0100

    get posts action created

---
+++ client/src/redux/actions/postActions.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+import {
+  POSTS_REQUEST,
+  POSTS_SUCCESS,
+  POSTS_ERROR,
+} from '../constants/postConstants';
+
+export const getPosts = () => async (dispatch) => {
+  try {
+    dispatch({ type: POSTS_REQUEST });
+
+    const { data } = axios.get('/posts');
+
+    dispatch({ type: POSTS_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: POSTS_ERROR,
+      payload: error.message,
+    });
+  }
+};
